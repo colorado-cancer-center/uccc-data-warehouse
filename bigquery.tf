ff8edd190c2a5569009513710c4c3af409b3b276
Sean Davis <seandavi@gmail.com>
2022-11-15 04:43:27 -0700

    refactor some bigquery into variables

--- bigquery.tf
+++ bigquery.tf
@@ -1,7 +1,22 @@
+variable "owner_email" { 
+  type = string
+  description = "Email address of the owner of the bigquery dataset"
+}
+
+variable "dataset_name" { 
+  type = string
+  description = "Name of the bigquery dataset"
+}
+
+variable "dataset_description" { 
+  type = string
+  description = "Description of the bigquery dataset"
+}
+
 resource "google_bigquery_dataset" "dataset" {
-  dataset_id                  = "genomics"
-  friendly_name               = "genomics"
-  description                 = "Clinical genomics data for patients from the UCHealth system"
+  dataset_id                  = "${var.dataset_name}"
+  friendly_name               = "${var.dataset_name}"
+  description                 = "${var.dataset_description}"
   location                    = "US"
   project = "${google_project.project.project_id}"
   labels = {
@@ -12,11 +27,12 @@ resource "google_bigquery_dataset" "dataset" {
 
   access {
     role          = "OWNER"
-    user_by_email = "seandavi@gmail.com"
+    user_by_email = "${var.owner_email}"
   }
 
 }
 
 output "dataset_id" {
   value = "${google_bigquery_dataset.dataset.dataset_id}"
+  description = "The google bigquery dataset id"
 }
